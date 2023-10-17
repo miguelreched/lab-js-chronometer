9d2207c1a4926ab9ec7d3d74a3c692ff6b36b1c3
miguelreched <121823965+miguelreched@users.noreply.github.com>
2023-10-17 20:24:00 +0200

    lab-terminado

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,69 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null 
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(()=>{
+      this.currentTime++
+      if (printTimeCallback){
+        printTimeCallback()
+      }
+
+    },1000)
   }
 
   getMinutes() {
     // ... your code goes here
+    let minutos = this.currentTime / 60;
+    return Math.floor(minutos)
+    // let minutosRound = Math.floor(minutos)
   }
 
   getSeconds() {
     // ... your code goes here
+    let segundos = this.currentTime % 60;
+    return Math.floor(segundos)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+  //   // ... your code goes here
+    
+  //   // let dosDi = value.toString()
+    
+    if( value === 0){
+      return "00" 
+    }
+    else if(value > 0 && value <=9){
+      return "0"+value
+    }
+    else {
+      return value.toString()
+    }
   }
 
   stop() {
     // ... your code goes here
-  }
+    clearInterval( this.intervalId )
+    }
+
+  
+  
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0
+
+
   }
 
   split() {
     // ... your code goes here
+    let segundos = this.computeTwoDigitNumber (this.getSeconds())
+    let minutos = this.computeTwoDigitNumber (this.getMinutes())
+    return `${minutos}:${segundos}`
   }
 }
